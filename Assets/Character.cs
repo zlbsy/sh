875b6424cbb5febe2fe969ae28d3681aed2e36ea
lufy <lufy.legend@gmail.com>
2016-10-23 23:31:41 +0900

    character animation

---
+++ Assets/Character.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+	[SerializeField]private Animator animation;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void SetAction(string action){
+		animation.Play (action);
+	}
+}
---
+++ Assets/CharacterTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterTest : MonoBehaviour {
+	[SerializeField] Character character;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnGUI(){
+		if(GUI.Button(new Rect(100, 50, 100, 30), "Idle")){
+			character.SetAction ("Idle");
+		}
+		if(GUI.Button(new Rect(100, 100, 100, 30), "Move")){
+			character.SetAction ("Move");
+		}
+		if(GUI.Button(new Rect(100, 150, 100, 30), "Attack")){
+			character.SetAction ("Attack");
+		}
+	}
+}
