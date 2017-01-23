bc05b9d530de269ef13f05dfd578e95a6747df0e
lufy <lufy.legend@gmail.com>
2017-01-24 02:07:09 +0900

    update

---
+++ Assets/Script/App/Controller/CTop.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using App.Service;
+using App.Model;
+using App.View;
+
+
+namespace App.Controller{
+	public class CTop : CBase {
+		public override IEnumerator OnLoad( ) 
+		{  
+			yield return 0;
+		}
+	}
+}
