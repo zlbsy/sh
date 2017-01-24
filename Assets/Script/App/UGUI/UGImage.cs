878dd2ec3748bc37a151bd1dcd1a07b153e21250
張 路斌 <lubin.zhang@P0504-M.local>
2017-01-24 20:08:48 +0900

    update

---
+++ Assets/Script/App/UGUI/UGImage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace App.UGUI{
+    public class UGImage: UnityEngine.UI.Image {
+        public bool tofullScreen = false;
+        void Start()
+        {
+            if (tofullScreen)
+            {
+                int imgWidth = this.mainTexture.width;
+                int imgHeight = this.mainTexture.height;
+                int width = Screen.width;
+                int height = Screen.height;
+                this.rectTransform.sizeDelta = new Vector2(width, imgHeight * width / imgWidth);
+            }
+        }
+	}
+}
---
+++ Assets/Script/App/UGUI/UGImageEditor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace App.UGUI{
+    [CustomEditor(typeof(App.UGUI.UGImage))]
+    public class UGImageEditor: Editor {
+        //private SerializedProperty tofullScreenProp = null;
+        void OnEnable () {
+            //tofullScreenProp = serializedObject.FindProperty ("tofullScreen");
+        }
+        /*public override void OnInspectorGUI ()
+        {
+            base.OnInspectorGUI();
+            serializedObject.Update ();
+            EditorGUILayout.BeginHorizontal();
+            //tofullScreenProp.boolValue = EditorGUILayout.Toggle("tofullScreen", tofullScreenProp.boolValue, GUILayout.MinWidth(10f));
+            EditorGUILayout.EndHorizontal();
+            serializedObject.ApplyModifiedProperties ();
+        }*/
+	}
+}
