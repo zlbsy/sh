d94741bbac59e8b5a4294c632ccf6128532854e1
張 路斌 <lubin.zhang@P0504-M.local>
2017-02-17 20:36:07 +0900

    character

--- Assets/Script/App/Model/Master/MCharacter.cs
+++ Assets/Script/App/Model/Master/MCharacter.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using App.ViewModel;
 
 
@@ -12,8 +11,27 @@ namespace App.Model.Master{
 		public int hp;//
 		public int mp;//
         public int sp;//怒气
-        public int power;//力量
         public int force;//武力
+        public int intelligence;//智力
+        public int command;//统率
+        public int agility;//敏捷
+        public int luck;//运气
+        public int power;//力量
+        public int moving_power;//轻功
+        public int riding;//骑术
+        public int walker;//步战
+        public int pike;//长枪
+        public int sword;//短剑
+        public int long_knife;//大刀
+        public int knife;//短刀
+        public int long_ax;//长斧
+        public int ax;//短斧
+        public int sticks;//棍棒
+        public int fist;//拳脚
+        public int archery;//箭术
+        public int hidden_weapons;//暗器
+        public int dual_wield;//双手
+        public int face_rect;//小头像范围
 
 
 	}
