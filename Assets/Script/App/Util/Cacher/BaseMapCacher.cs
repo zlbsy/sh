70d6197e01e10540b4d40b097f25f58d0ab0a3c5
zlb <zhanglubinsy@yahoo.co.jp>
2017-02-23 13:29:18 +0900

    delete client

--- Assets/Script/App/Util/Cacher/BaseMapCacher.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-namespace App.Util.Cacher{
-    public class BaseMapCacher: CacherBase<BaseMapCacher, App.Model.Master.MBaseMap> {}
-}
