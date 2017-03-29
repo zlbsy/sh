d33d23f814f71dc49658d10197d79aa2af3d6046
lufy <lufy.legend@gmail.com>
2017-03-30 01:19:27 +0900

    battle skill

--- Assets/Script/App/View/Battlefield/VBattleSkillIcon.cs
+++ Assets/Script/App/View/Battlefield/VBattleSkillIcon.cs
@@ -9,18 +9,24 @@ using App.Util.Cacher;
 using App.Controller;
 
 namespace App.View.Battlefield{
-    public class VBattleSkillIcon : VBase {
+    public class VBattleSkillIcon : VBase, UnityEngine.EventSystems.IPointerDownHandler, UnityEngine.EventSystems.IPointerUpHandler, UnityEngine.EventSystems.IPointerClickHandler {
         [SerializeField]private Image icon;
         [SerializeField]private Text mp;
         #region VM处理
         public VMSkill ViewModel { get { return (VMSkill)BindingContext; } }
         public override void UpdateView(){
             icon.sprite = ImageAssetBundleManager.GetSkillIcon(ViewModel.SkillId.Value);
-            mp.text = "14";
+            mp.text = string.Format("M {0}", 14);
         }
         #endregion
-        public void ClickChild(){
+        public void OnPointerClick(UnityEngine.EventSystems.PointerEventData eventData){
             this.Controller.SendMessage("SkillIconClick", ViewModel.SkillId.Value);
+        }
+        public void OnPointerDown (UnityEngine.EventSystems.PointerEventData eventData) {
+            this.Controller.SendMessage("ShowPreview", ViewModel.SkillId.Value);
+        }
+        public void OnPointerUp (UnityEngine.EventSystems.PointerEventData eventData) {
+            this.Controller.SendMessage("HidePreview");
         }
     }
 }
