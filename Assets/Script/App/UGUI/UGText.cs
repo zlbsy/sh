db5dcb0b867cfec2b41cb1fa4e6ac6bb63235eb2
張 路斌 <lubin.zhang@P0504-M.local>
2017-08-31 10:16:39 +0900

    editor分离

--- Assets/Script/App/UGUI/UGText.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace App.UGUI{
-    public class UGText: UnityEngine.UI.Text {
-	}
-}
--- Assets/Script/App/Util/Cacher/CharacterCacher.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-
-namespace App.Util.Cacher{
-    //武将Master数据
-    public class CharacterCacher: CacherBase<CharacterCacher, App.Model.Master.MCharacter> {}
-}
